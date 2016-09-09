bcd45a0a5edd68c8a4c28758c23478cfe4741ba1
John <jmoriarty99@gmail.com>
2016-09-09 19:12:34 -0400

    9/9/2016

---
+++ Miwok V2/app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -0,0 +1,34 @@
+package com.example.android.miwok;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class ColorsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle(R.string.category_colors);
+        setContentView(R.layout.activity_colors);
+
+        ArrayList<Word> words = new ArrayList<Word>();
+
+        words.add(new Word("one", "lutti"));
+        words.add(new Word("two", "otiiko"));
+        words.add(new Word("three", "tolookosu"));
+        words.add(new Word("four", "oyyisa"));
+        words.add(new Word("five", "massokka"));
+        words.add(new Word("six", "temmokka"));
+        words.add(new Word("seven", "kenekaku"));
+        words.add(new Word("eight", "kawinta"));
+        words.add(new Word("nine", "wo'e"));
+        words.add(new Word("ten", "na'aacha"));
+
+        WordAdapter adapter = new WordAdapter(this, words, R.color.category_colors);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
+    }
+}
---
+++ Miwok/app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,68 @@
+package com.example.android.miwok;
+
+import android.app.Activity;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+/**
+ * Created by johnmoriarty on 9/4/16.
+ */
+public class WordAdapter extends ArrayAdapter<Word> {
+
+    private int mColorResourceId;
+
+    public WordAdapter(Activity context, ArrayList<Word> words, int colorResourceId) {
+        super(context, 0, words);
+        mColorResourceId = colorResourceId;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+
+        Word currentWord = getItem(position);
+
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+
+        miwokTextView.setText(currentWord.getMiwokTranslation());
+
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
+
+        if(currentWord.hasImage()){
+            //Set the ImageView to the image resource specified in the cuurent Word
+            imageView.setImageResource(currentWord.getImageResourceId());
+
+            //Make sure the view is visible
+            imageView.setVisibility(View.VISIBLE);
+        }
+        else{
+            //otherwise hid the Image View (set visibility to GONE)
+            imageView.setVisibility(View.GONE);
+        }
+
+        View textContainer = listItemView.findViewById(R.id.text_container);
+
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+
+        textContainer.setBackgroundColor(color);
+
+        return listItemView;
+    }
+}
